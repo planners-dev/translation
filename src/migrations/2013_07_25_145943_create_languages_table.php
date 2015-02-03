1e4235a0920774931ba75c164cfe7894b0e08226
AntyRace <antyrace@antyrace.com>
2015-02-03 16:03:57 +0100

    update: protection from unnessesery deletion of languages;

--- src/migrations/2013_07_25_145943_create_languages_table.php
+++ src/migrations/2013_07_25_145943_create_languages_table.php
@@ -2,32 +2,32 @@
 
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLanguagesTable extends Migration {
+class CreateLanguagesTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('languages', function($table){
-			$table->engine = 'InnoDB';
-			$table->increments('id');
-			$table->string('locale', 6)->unique();
-			$table->string('name', 60)->unique();
-			$table->timestamps();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('languages', function ($table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('locale', 6)->unique();
+            $table->string('name', 60)->unique();
+            $table->timestamps();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('languages');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
 
 }
--- src/migrations/2013_07_25_145958_create_language_entries_table.php
+++ src/migrations/2013_07_25_145958_create_language_entries_table.php
@@ -2,38 +2,38 @@
 
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLanguageEntriesTable extends Migration {
+class CreateLanguageEntriesTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('language_entries', function($table){
-			$table->engine = 'InnoDB';
-			$table->increments('id');
-			$table->integer('language_id')->unsigned();
-			$table->string('namespace', 150)->default('*');
-			$table->string('group', 150);
-			$table->string('item', 150);
-			$table->text('text');
-			$table->boolean('unstable')->default('0');
-			$table->timestamps();
-			$table->foreign('language_id')->references('id')->on('languages');
-			$table->unique(array('language_id', 'namespace', 'group', 'item'));
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('language_entries', function ($table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->integer('language_id')->unsigned();
+            $table->string('namespace', 150)->default('*');
+            $table->string('group', 150);
+            $table->string('item', 150);
+            $table->text('text');
+            $table->boolean('unstable')->default('0');
+            $table->timestamps();
+            $table->foreign('language_id')->references('id')->on('languages');
+            $table->unique(array('language_id', 'namespace', 'group', 'item'));
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('language_entries');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
 
 }
